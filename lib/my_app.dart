a4241ce8b00039f933bc527dcae8b2a8d5213ed3
Paulo Rodriguez <paulo_crr@hotmail.com>
2023-05-13 12:59:00 -0500

    feat: Se agrego la libreria de flexthemes

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,3 +1,4 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:whats_app_phones/pages/my_home_page.dart';
 
@@ -9,10 +10,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      theme: FlexThemeData.light(
+        colors: const FlexSchemeColor(
+          primary: Color(0xff065808),
+          primaryContainer: Color(0xff9bbc9c),
+          secondary: Color(0xff365b37),
+          secondaryContainer: Color(0xffaebdaf),
+          tertiary: Color(0xff2c7e2e),
+          tertiaryContainer: Color(0xffb8e6b9),
+          appBarColor: Color(0xffb8e6b9),
+          error: Color(0xffb00020),
+        ),
+        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+        blendLevel: 9,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 10,
+          blendOnColors: false,
+        ),
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
         useMaterial3: true,
+        swapLegacyOnMaterial3: true,
       ),
+      darkTheme: FlexThemeData.dark(
+        colors: const FlexSchemeColor(
+          primary: Color(0xff629f80),
+          primaryContainer: Color(0xff273f33),
+          secondary: Color(0xff81b39a),
+          secondaryContainer: Color(0xff4d6b5c),
+          tertiary: Color(0xff88c5a6),
+          tertiaryContainer: Color(0xff356c50),
+          appBarColor: Color(0xff356c50),
+          error: Color(0xffcf6679),
+        ),
+        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+        blendLevel: 15,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 20,
+        ),
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        useMaterial3: true,
+        swapLegacyOnMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
       home: const MyHomePage(),
     );
   }
